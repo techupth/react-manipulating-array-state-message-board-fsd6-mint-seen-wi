783294d887babba360fddafe9c2b0c7be4de5e1c
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-06-06 13:47:37 +0700

    feat: added listing feature

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,24 @@
+import {useState} from "react"
+
 function MessageBoard() {
+
+  const [collectedMessage, setCollectedMessage] = useState(null)
+  const [message, setMessage] = useState([])
+
+  const displayMessage = (event) => {
+    event.preventDefault();
+    const newMessage = [...message];
+    newMessage.push(collectedMessage)
+    setMessage(newMessage)
+    setCollectedMessage("")
+  }
+
+  const deleteMessage = (index) => {
+    const newMessage = [...message];
+    newMessage.splice(index, 1)
+    setMessage(newMessage)
+  }
+  
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +29,26 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={collectedMessage}
+            onChange={(event) => {
+              setCollectedMessage(event.target.value)
+            }}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={displayMessage}>Submit</button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+              
+          <ul>
+        {message.map((item,index) => {
+            return (
+            <div className="message">
+              <h1 key={index}>{item}<button className="delete-button" onClick={() => {
+                deleteMessage(index)
+              }}>x</button></h1>          
+             </div>)
+             })}
+            </ul>  
       </div>
     </div>
   );
